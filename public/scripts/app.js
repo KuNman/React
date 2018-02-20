c26c686bad27b75df99d78b2c0c1725ea0c2a5e9
KuNman <knejman56@gmail.com>
2018-02-20 21:01:40 +0100

    JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,49 @@
-"use strict";
+'use strict';
 
 console.log("App.js is running");
 
 //JSX
+var app = {
+  title: '1st React app',
+  subtitle: 'subtitle'
+};
+
 var template = React.createElement(
-  "h1",
+  'div',
   null,
-  "JSX app.js --watch"
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  )
 );
+
+var userName = 'Michael';
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    userName
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 99'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location : Poland'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 console.log("App.js is running");
 
 //JSX
-var template = <h1>JSX app.js --watch</h1>;
+var app = {
+    title: '1st React app',
+    subtitle: 'subtitle'
+};
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
+);
+
+var userName = 'Michael';
+var templateTwo = (
+  <div>
+    <h1>{userName}</h1>
+    <p>Age: 99</p>
+    <p>Location : Poland</p>
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
