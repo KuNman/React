972d8f74d36c990ed79aec9c05b83d1063093e3a
KuNman <knejman56@gmail.com>
2018-02-26 02:31:29 +0100

    nested react component

--- src/app.js
+++ src/app.js
@@ -1,57 +1,65 @@
-console.log("App.js is running");
-const appRoot = document.getElementById('app');
-
-//JSX
-const app = {
-    title: '1st React app',
-    subtitle: 'subtitle',
-    options: [],
-};
-
-function getSubtitle(subtitle) {
-  if(subtitle) {
-    return <p>{subtitle}</p>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Action/>
+        <Options/>
+        <AddOption/>
+      </div>
+    );
   }
 }
 
-const onFormSubmit = (e) => {
-  e.preventDefault();
-
-  const option = e.target.elements.inputs.value;
-
-  if(option) {
-    app.options.push(option);
-    e.target.elements.inputs.value = '';
-    renderTemplate();
-  }
-};
-
-const clearOptions = () => {
-
-  if (app.options.length >= 1) {
-    app.options = [];
-    renderTemplate();
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
   }
 }
 
-const renderTemplate = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {getSubtitle(app.subtitle)}
-      {app.options.length > 0 ? <p>{app.options.length}</p> : <h3>No options</h3>}
-      { app.options.map((option) => {
-         return <li key={option}>Option: {option}</li>;
-        })
-      }
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="inputs" required/>
-        <button> Add option</button>
-      </form>
-      <button onClick={clearOptions}>Wipe array</button>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
 }
 
-renderTemplate();
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        Options here
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option
+      </div>
+    )
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Add option</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
